7c2c52f6d026e0c62af583d7ee45f0e3e9ef8c13
Chira Cosmin <146840512+ChiraCosminFlorian@users.noreply.github.com>
2025-12-15 17:41:50 +0200

    Add files via upload

---
+++ LAB8/Problema1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBarIterations = new System.Windows.Forms.TrackBar();
+            this.labelIterations = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarIterations)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(611, 426);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // trackBarIterations
+            // 
+            this.trackBarIterations.Location = new System.Drawing.Point(682, 112);
+            this.trackBarIterations.Name = "trackBarIterations";
+            this.trackBarIterations.Size = new System.Drawing.Size(104, 56);
+            this.trackBarIterations.TabIndex = 1;
+            // 
+            // labelIterations
+            // 
+            this.labelIterations.AutoSize = true;
+            this.labelIterations.Location = new System.Drawing.Point(694, 185);
+            this.labelIterations.Name = "labelIterations";
+            this.labelIterations.Size = new System.Drawing.Size(91, 16);
+            this.labelIterations.TabIndex = 2;
+            this.labelIterations.Text = "labelIterations";
+            this.labelIterations.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelIterations);
+            this.Controls.Add(this.trackBarIterations);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarIterations)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBarIterations;
+        private System.Windows.Forms.Label labelIterations;
+    }
+}
+
